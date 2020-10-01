820b4565b6f1bc192924ed7ad4c3c6aa102724a2
Thari zephaniah <56599367+Zeph-T@users.noreply.github.com>
2020-10-01 23:44:50 +0530

    Create Zeph-T.js
    Added File

---
+++ peoples/Zeph-T.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Thari Zephaniah',
+  github: 'Zeph-T',
+  email: 'paul.zeph@gmail.com',
+  twitter: '@xxx',
+  facebook: 'Thari Zephaniah',
+  linkedin: 'in/Thari Zephaniah'
+};
